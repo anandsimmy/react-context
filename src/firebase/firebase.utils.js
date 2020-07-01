f8238e522c7395da8aef7967a6c040b006068cf2
Anand Simmy <anandsimmy7@gmail.com>
2020-07-01 11:54:49 +0530

    initial commit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyBY-84owDZBYNheFRHTv9epCpVC89UWOvE",
+  authDomain: "crown-db-66a57.firebaseapp.com",
+  databaseURL: "https://crown-db-66a57.firebaseio.com",
+  projectId: "crown-db-66a57",
+  storageBucket: "crown-db-66a57.appspot.com",
+  messagingSenderId: "646898735149",
+  appId: "1:646898735149:web:f5d7ade92dca402fe700a4",
+  measurementId: "G-MJ7GHK0MH8"
 };
 
 firebase.initializeApp(config);
