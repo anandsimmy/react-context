7ffeba75c2c5687a23a355a1dc06749dfd63f5e8
Anand Simmy <anandsimmy7@gmail.com>
2020-07-10 17:21:04 +0530

    replaced redux with context-api

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,20 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
-import { selectCartHidden } from '../../redux/cart/cart.selectors';
+
 import CurrentUserContext from '../../contexts/user/user-context'
+import CartContext from '../../contexts/cart/cart-context'
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ hidden }) => {
+const Header = () => {
   
+  const [hidden, setHidden]= useState(true)
+  const toggleCartHidden= () => setHidden(!hidden)
   const currentUser= useContext(CurrentUserContext)
 
   return (
@@ -38,14 +39,12 @@ const Header = ({ hidden }) => {
           SIGN IN
         </Link>
       )}
-      <CartIcon />
+      <CartContext.Provider value={{ hidden, toggleCartHidden }}>
+        <CartIcon />
+      </CartContext.Provider>
     </div>
     {hidden ? null : <CartDropdown />}
   </div>
 )};
 
-const mapStateToProps = createStructuredSelector({
-  hidden: selectCartHidden
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
